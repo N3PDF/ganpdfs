97b3bcfc3fb379279bc9f37f601b7ac2eb11a165
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2019-07-02 19:16:57 +0200

    Simplified Vanilla model and implement preprocessing

--- src/xganpdfs/model.py
+++ src/xganpdfs/model.py
@@ -21,115 +21,101 @@ class vanilla_xgan_model(object):
     def __init__(self, noise_size, output_size, x_pdf, params, activ, optmz):
         self.noise_size  = noise_size
         self.output_size = output_size
-        self.G   = None
-        self.D   = None
-        self.G_M = None
-        self.D_M = None
-        self.GAN_M = None
         self.x_pdf = x_pdf
-
         self.params  = params
         self.activ   = activ
         self.optmz   = optmz
         self.g_nodes = params['g_nodes']
         self.d_nodes = params['d_nodes']
-        self.options = None
 
-        self.generator     = self.generator_model()
+        # Timeline save
+        if params['timeline']:
+            self.options  = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            self.metadata = tf.RunMetadata()
+        else:
+            self.options  = None
+            self.metadata = None
+
+        #---------------------------#
+        #       DISCRIMINATOR       #
+        #---------------------------#
+        disc_optimizer = self.optmz[params['d_opt']]
         self.discriminator = self.discriminator_model()
-        self.gan           = self.gan_model()
+        self.discriminator.compile(loss=params['d_loss'], 
+                optimizer=disc_optimizer, metrics=['accuracy'])
+        self.discriminator.name = 'Discriminator'
+        self.discriminator.summary()
 
-    def generator(self):
+        #---------------------------#
+        #         GENERATOR         # 
+        #---------------------------#
+        G_input = Input(shape=(noise_size,))
+        self.generator = self.generator_model()
+        self.generator.name = 'Generator'
+        self.generator.summary()
+
+        #---------------------------#
+        #     ADVERSARIAL MODEL     #
+        #---------------------------#
+        """
+        Set Discriminator training to be fasle by default.
+        Only set to true when the discriminator is trained.
+        """
+        self.discriminator.trainable = False
+        fake_pdf = self.generator(G_input)
+        validity = self.discriminator(fake_pdf)
+        gan_optimizer = self.optmz[params['gan_opt']]
+
+        self.gan = Model(G_input, validity)
+        self.gan.compile(loss=params['gan_loss'], optimizer=gan_optimizer,
+                        metrics=['accuracy'], options=self.options,
+                        run_metadata=self.metadata)
+        self.gan.name = 'GAN'
+        self.gan.summary()
+
+    def generator_model(self):
 
         # Input of G/Random noises
         G_input = Input(shape=(self.noise_size,))
 
         # 1st hidden dense layer
-        G_in = Dense(self.g_nodes//4)(G_input)
-        G_in = self.activ[self.params['g_act']](G_in)
+        G_1l = Dense(self.g_nodes//4)(G_input)
+        G_1a = self.activ[self.params['g_act']](G_1l)
         # 2nd hidden custom layer with x-grid
-        G_in = xlayer(self.g_nodes//2, self.x_pdf)(G_in)
-        G_in = self.activ[self.params['g_act']](G_in)
+        G_2l = xlayer(self.g_nodes//2, self.x_pdf)(G_1a)
+        G_2a = self.activ[self.params['g_act']](G_2l)
         # 3rd hidden dense layer
-        G_in = Dense(self.g_nodes)(G_in)
-        G_in = self.activ[self.params['g_act']](G_in)
+        G_3l = Dense(self.g_nodes)(G_2a)
+        G_3a = self.activ[self.params['g_act']](G_3l)
         # 4th hidden dense layer
-        G_in = Dense(self.g_nodes*2)(G_in)
-        G_in = self.activ[self.params['g_act']](G_in)
-        # Output layer
-        G_output = Dense(self.output_size, activation='sigmoid')(G_in)
-        # xpreproc = preprocessing(self.x_pdf, 0.2, 6)
-        # G_output = K.multitply([G_output, xpreproc.compute()])
+        G_4l = Dense(self.g_nodes*2)(G_3a)
+        G_4a = self.activ[self.params['g_act']](G_4l)
+        # Compute the actual PDF
+        G_5l = Dense(self.output_size, activation='sigmoid')(G_4a)
+        # Output layer with preprocessing
+        G_output = preprocessing(self.x_pdf, 0.2, 6)(G_5l)
 
-        self.G = Model(G_input, G_output)
+        return Model(G_input, G_output)
 
-        return self.G
-
-    def discriminator(self):
+    def discriminator_model(self):
 
         # Input of D/Output of G
         D_input = Input(shape=(self.output_size,))
 
         # 1st hidden dense layer
-        D_in = Dense(self.d_nodes)(D_input)
-        D_in = self.activ[self.params['d_act']](D_in)
+        D_1l = Dense(self.d_nodes)(D_input)
+        D_1a = self.activ[self.params['d_act']](D_1l)
         # 2nd hidden dense layer
-        D_in = Dense(self.d_nodes//2)(D_in)
-        D_in = self.activ[self.params['d_act']](D_in)
+        D_2l = Dense(self.d_nodes//2)(D_1a)
+        D_2a = self.activ[self.params['d_act']](D_2l)
         # # 3rd hidden dense layer
         # D_in = Dense(self.d_nodes//8)(D_in)
         # D_in = self.activ[self.params['d_act']](D_in)
 
         # Output 1 dimensional probability
-        D_output = Dense(1, activation='sigmoid')(D_in)
+        D_output = Dense(1, activation='sigmoid')(D_2a)
 
-        self.D = Model(D_input, D_output)
-
-        return self.D
-
-    def generator_model(self):
-
-        gen_optimizer = self.optmz[self.params['g_opt']]
-
-        self.G_M = Sequential()
-        self.G_M.add(self.generator())
-        self.G_M.compile(loss=self.params['g_loss'], optimizer=gen_optimizer,
-                        metrics=['accuracy'])
-        self.G_M.name = 'Generator'
-        self.G.summary()
-
-        return self.G_M
-
-    def discriminator_model(self):
-
-        disc_optimizer = self.optmz[self.params['d_opt']]
-
-        self.D_M = Sequential()
-        self.D_M.add(self.discriminator())
-        self.D_M.compile(loss=self.params['d_loss'], optimizer=disc_optimizer,
-                        metrics=['accuracy'])
-        self.D_M.name = 'Discriminator'
-        self.D.summary()
-
-        return self.D_M
-
-    def gan_model(self):
-
-        gan_optimizer = self.optmz[self.params['gan_opt']]
-
-        Gan_input  = Input(shape=(self.noise_size,))
-        Gan_latent = self.G_M(Gan_input)
-        Gan_output = self.D_M(Gan_latent)
-        self.GAN_M = Model(Gan_input, Gan_output)
-
-        self.D_M.trainable = False
-
-        self.GAN_M.name = 'GAN'
-        self.GAN_M.compile(loss=self.params['gan_loss'], optimizer=gan_optimizer,
-                        metrics=['accuracy'])
-        self.GAN_M.summary()
-
-        return self.GAN_M
+        return Model(D_input, D_output)
 
 
 class dc_xgan_model(object):
@@ -145,16 +131,12 @@ class dc_xgan_model(object):
     def __init__(self, noise_size, output_size, x_pdf, params, activ, optmz):
         self.noise_size  = noise_size
         self.output_size = output_size
-        self.x_pdf = x_pdf
-
+        self.x_pdf   = x_pdf
         self.params  = params
         self.activ   = activ
         self.optmz   = optmz
         self.g_nodes = params['g_nodes']
         self.d_nodes = params['d_nodes']
-
-        disc_optimizer = self.optmz[self.params['d_opt']]
-        G_input = Input(shape=(noise_size,))
 
         # Timeline save
         if params['timeline']:
@@ -164,31 +146,39 @@ class dc_xgan_model(object):
             self.options  = None
             self.metadata = None
 
-        # Construct the models
+        #---------------------------#
+        #       DISCRIMINATOR       #
+        #---------------------------#
+        disc_optimizer = self.optmz[params['d_opt']]
         self.discriminator = self.discriminator_model()
-        # self.discriminator.compile(loss=self.params['d_loss'], optimizer=disc_optimizer,
-        #         options=self.options, run_metadata=self.metadata, metrics=['accuracy'])
-        self.discriminator.compile(loss=self.params['d_loss'], optimizer=disc_optimizer,
+        # self.discriminator.compile(loss=params['d_loss'], optimizer=disc_optimizer,
+        #         metrics=['accuracy'], options=self.options, 
+        #         run_metadata=self.metadata, metrics=['accuracy'])
+        self.discriminator.compile(loss=params['d_loss'], optimizer=disc_optimizer,
                                     metrics=['accuracy'])
+
+        #---------------------------#
+        #         GENERATOR         # 
+        #---------------------------#
+        G_input = Input(shape=(noise_size,))
         self.generator = self.generator_model()
 
-        # Generate fake PDFs
-        fake = self.generator(G_input)
-
+        #---------------------------#
+        #     ADVERSARIAL MODEL     #
+        #---------------------------#
         """
         Set Discriminator training to be false.
         Only set to true when the discriminator is trained.
         """
+        fake = self.generator(G_input)
         self.discriminator.trainable = False
-
-        # Discriminator takes the fake PDFs and determines its validity
         validity = self.discriminator(fake)
 
-        # GAN/Combined model
         gan_optimizer = self.optmz[self.params['gan_opt']]
         self.gan = Model(G_input, validity)
-        self.gan.compile(loss=params['gan_loss'], optimizer=gan_optimizer,
-                metrics=['accuracy'], options=self.options, run_metadata=self.metadata)
+        self.gan.compile(loss=params['gan_loss'], optimizer=gan_optimizer, 
+                metrics=['accuracy'], options=self.options, 
+                run_metadata=self.metadata)
 
 
     def generator_model(self):
