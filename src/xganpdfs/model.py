0eacf81a775e682ce25ece423154b26645d551dd
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2019-09-12 16:40:41 +0200

    Removed unused GAN model and timeliner

--- src/xganpdfs/model.py
+++ src/xganpdfs/model.py
@@ -13,9 +13,9 @@ class vanilla_xgan_model(object):
     """
     Generative Adversarial Neural Network Model.
     * Discriminator::Learn the features of the true data and
-    and check whether or not the generated data ressembles to the true.
+      and check whether or not the generated data ressembles to the true.
     * Generator::Learn to generate fake data by tricking the Discriminator.
-    * GAN::Do the adversarial training.
+    * GAN::Manage the adversarial training.
     """
 
     def __init__(self, noise_size, output_size, x_pdf, params, activ, optmz):
@@ -28,21 +28,14 @@ class vanilla_xgan_model(object):
         self.g_nodes = params['g_nodes']
         self.d_nodes = params['d_nodes']
 
-        # Timeline save
-        if params['timeline']:
-            self.options  = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-            self.metadata = tf.RunMetadata()
-        else:
-            self.options  = None
-            self.metadata = None
-
         #---------------------------#
         #       DISCRIMINATOR       #
         #---------------------------#
         disc_optimizer = self.optmz[params['d_opt']]
         self.discriminator = self.discriminator_model()
         self.discriminator.compile(loss=params['d_loss'],
-                optimizer=disc_optimizer, metrics=['accuracy'])
+                                   optimizer=disc_optimizer,
+                                   metrics=['accuracy'])
         self.discriminator.name = 'Discriminator'
         self.discriminator.summary()
 
@@ -67,15 +60,18 @@ class vanilla_xgan_model(object):
         gan_optimizer = self.optmz[params['gan_opt']]
 
         self.gan = Model(G_input, validity)
-        self.gan.compile(loss=params['gan_loss'], optimizer=gan_optimizer,
-                        metrics=['accuracy'], options=self.options,
-                        run_metadata=self.metadata)
+        self.gan.compile(loss=params['gan_loss'],
+                         optimizer=gan_optimizer,
+                         metrics=['accuracy'])
         self.gan.name = 'GAN'
         self.gan.summary()
 
     def generator_model(self):
-
-        # Input of G/Random noises
+        """
+        This construct the architercture
+        of the Generator. 
+        """
+        # Input of G/Random noise of 1 dim vector
         G_input = Input(shape=(self.noise_size,))
 
         # 1st hidden dense layer
@@ -98,8 +94,16 @@ class vanilla_xgan_model(object):
         return Model(G_input, G_output)
 
     def discriminator_model(self):
+        """
+        This construct the architecture
+        of the Discriminator.
+        """
 
-        # Input of D/Output of G
+        """
+        Input of D/Output of G.
+        This is a 1 dim vector of the
+        size of the x-gird.
+        """
         D_input = Input(shape=(self.output_size,))
 
         # 1st hidden dense layer
@@ -116,108 +120,3 @@ class vanilla_xgan_model(object):
         D_output = Dense(1, activation='sigmoid')(D_2a)
 
         return Model(D_input, D_output)
-
-
-class dc_xgan_model(object):
-
-    """
-    Generative Adversarial Neural Network Model.
-    * Discriminator::Learn the features of the true data and
-    and check whether or not the generated data ressembles to the true.
-    * Generator::Learn to generate fake data by tricking the Discriminator.
-    * GAN::Do the adversarial training.
-    """
-
-    def __init__(self, noise_size, output_size, x_pdf, params, activ, optmz):
-        self.noise_size  = noise_size
-        self.output_size = output_size
-        self.x_pdf   = x_pdf
-        self.params  = params
-        self.activ   = activ
-        self.optmz   = optmz
-        self.g_nodes = params['g_nodes']
-        self.d_nodes = params['d_nodes']
-
-        # Timeline save
-        if params['timeline']:
-            self.options  = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
-            self.metadata = tf.RunMetadata()
-        else:
-            self.options  = None
-            self.metadata = None
-
-        #---------------------------#
-        #       DISCRIMINATOR       #
-        #---------------------------#
-        disc_optimizer = self.optmz[params['d_opt']]
-        self.discriminator = self.discriminator_model()
-        # self.discriminator.compile(loss=params['d_loss'], optimizer=disc_optimizer,
-        #         metrics=['accuracy'], options=self.options,
-        #         run_metadata=self.metadata, metrics=['accuracy'])
-        self.discriminator.compile(loss=params['d_loss'], optimizer=disc_optimizer,
-                                    metrics=['accuracy'])
-
-        #---------------------------#
-        #         GENERATOR         #
-        #---------------------------#
-        G_input = Input(shape=(noise_size,))
-        self.generator = self.generator_model()
-
-        #---------------------------#
-        #     ADVERSARIAL MODEL     #
-        #---------------------------#
-        """
-        Set Discriminator training to be false.
-        Only set to true when the discriminator is trained.
-        """
-        fake = self.generator(G_input)
-        self.discriminator.trainable = False
-        validity = self.discriminator(fake)
-
-        gan_optimizer = self.optmz[self.params['gan_opt']]
-        self.gan = Model(G_input, validity)
-        self.gan.compile(loss=params['gan_loss'], optimizer=gan_optimizer,
-                metrics=['accuracy'], options=self.options,
-                run_metadata=self.metadata)
-
-
-    def generator_model(self):
-
-        model = Sequential()
-
-        model.add(Dense(self.g_nodes//4, input_dim=self.noise_size))
-        model.add(self.activ[self.params['g_act']])
-        # model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(self.g_nodes//2))
-        model.add(self.activ[self.params['g_act']])
-        # model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(self.g_nodes))
-        model.add(self.activ[self.params['g_act']])
-        # model.add(BatchNormalization(momentum=0.8))
-        model.add(Dense(self.g_nodes*2))
-        model.add(self.activ[self.params['g_act']])
-        model.add(Dense(self.output_size))
-
-        model.summary()
-
-        noise = Input(shape=(self.noise_size,))
-        img = model(noise)
-
-        return Model(noise, img)
-
-    def discriminator_model(self):
-
-        model = Sequential()
-
-        # model.add(Flatten(input_shape=self.output_size))
-        model.add(Dense(self.d_nodes, input_dim=self.output_size))
-        model.add(self.activ[self.params['d_act']])
-        model.add(Dense(self.d_nodes//2))
-        model.add(self.activ[self.params['d_act']])
-        model.add(Dense(1, activation='sigmoid'))
-        model.summary()
-
-        img = Input(shape=(self.output_size,))
-        validity = model(img)
-
-        return Model(img, validity)
