4bee9c636f762b99dde902d8e27d6ba5abcbb19b
juacrumar <juacrumar@gmail.com>
2020-03-04 10:03:17 +0100

    reformar part of the code with black

--- src/wganpdfs/hyperscan.py
+++ src/wganpdfs/hyperscan.py
@@ -14,48 +14,50 @@ from wganpdfs.pdformat import xnodes
 from wganpdfs.train import xgan_train
 from wganpdfs.filetrials import FileTrials
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 def load_yaml(runcard_file):
     """Loads yaml runcard"""
-    with open(runcard_file, 'r') as stream:
+    with open(runcard_file, "r") as stream:
         runcard = yaml.load(stream)
     for key, value in runcard.items():
-        if 'hp.' in str(value):
+        if "hp." in str(value):
             runcard[key] = eval(value)
     return runcard
 
-#----------------------------------------------------------------------
+
+# ----------------------------------------------------------------------
 def run_hyperparameter_scan(search_space, max_evals, cluster, folder):
 
     """
     Run the hyperparameter scan using hyperopt.
     """
 
-    print('[+] Performing hyperparameter scan...')
+    print("[+] Performing hyperparameter scan...")
     if cluster:
-        trials = MongoTrials(cluster, exp_key='exp1')
+        trials = MongoTrials(cluster, exp_key="exp1")
     else:
         """
         Use constum trials in order to save the model after each trials.
         The following will generate a .json file containing the trials.
         """
         trials = FileTrials(folder, parameters=search_space)
-    best = fmin(hyper_train, search_space, algo=tpe.suggest,
-                max_evals=max_evals, trials=trials)
+    best = fmin(
+        hyper_train, search_space, algo=tpe.suggest, max_evals=max_evals, trials=trials
+    )
     # Save the overall best model
     best_setup = space_eval(search_space, best)
-    print('\n[+] Best scan setup:')
+    print("\n[+] Best scan setup:")
     pprint.pprint(best_setup)
-    with open('%s/best-model.yaml' % folder, 'w') as wfp:
+    with open("%s/best-model.yaml" % folder, "w") as wfp:
         yaml.dump(best_setup, wfp, default_flow_style=False)
-    log = '%s/hyperopt_log_{}.pickle'.format(time.time()) % folder
-    with open(log, 'wb') as wfp:
-        print(f'[+] Saving trials in {log}')
+    log = "%s/hyperopt_log_{}.pickle".format(time.time()) % folder
+    with open(log, "wb") as wfp:
+        print(f"[+] Saving trials in {log}")
         pickle.dump(trials.trials, wfp)
     return best_setup
 
 
-#----------------------------------------------------------------------
+# ----------------------------------------------------------------------
 def hyper_train(params):
 
     """
@@ -64,33 +66,41 @@ def hyper_train(params):
     # Load the x_grid
     X_PDF = xnodes().build_xgrid()
     # Define the number of input replicas
-    NB_INPUT_REP = params['input_replicas']
+    NB_INPUT_REP = params["input_replicas"]
     # Define the number of batches
     if NB_INPUT_REP < 10:
         BATCH_SIZE = 1
     else:
-        BATCH_SIZE = int(NB_INPUT_REP/10)
+        BATCH_SIZE = int(NB_INPUT_REP / 10)
 
     # Clear Keras session
     K.clear_session()
 
     # Model Parameters
     # List of activation funtions
-    activ = {'leakyrelu': LeakyReLU(alpha=0.2),
-             'elu': ELU(alpha=1.0),
-             'relu': ReLU()}
+    activ = {"leakyrelu": LeakyReLU(alpha=0.2), "elu": ELU(alpha=1.0), "relu": ReLU()}
     # List of optimization functions
-    optmz = {'sgd': SGD(lr=0.0075),
-             'rms': RMSprop(lr=0.00005),
-             'adadelta': Adadelta(lr=1.0)}
+    optmz = {
+        "sgd": SGD(lr=0.0075),
+        "rms": RMSprop(lr=0.00005),
+        "adadelta": Adadelta(lr=1.0),
+    }
 
-    xgan_pdfs = xgan_train(X_PDF, params['pdf'], 100, params, activ,
-                optmz, nb_replicas=NB_INPUT_REP, flavors=params['fl'])
+    xgan_pdfs = xgan_train(
+        X_PDF,
+        params["pdf"],
+        100,
+        params,
+        activ,
+        optmz,
+        nb_replicas=NB_INPUT_REP,
+        flavors=params["fl"],
+    )
 
     # In case one needs to pretrain the Discriminator
     # xgan_pdfs.pretrain_disc(BATCH_SIZE, epochs=4)
 
-    g_loss = xgan_pdfs.train(nb_epochs=params['epochs'],
-            batch_size=BATCH_SIZE,
-            verbose=params['verbose'])
-    return {'loss': g_loss, 'status': STATUS_OK}
+    g_loss = xgan_pdfs.train(
+        nb_epochs=params["epochs"], batch_size=BATCH_SIZE, verbose=params["verbose"]
+    )
+    return {"loss": g_loss, "status": STATUS_OK}
--- src/wganpdfs/run.py
+++ src/wganpdfs/run.py
@@ -1,6 +1,7 @@
 import os, argparse, shutil
 from wganpdfs.hyperscan import hyper_train, load_yaml, run_hyperparameter_scan
 
+
 def main():
     """
     This is the Main controller.
@@ -8,30 +9,45 @@ def main():
     """
 
     # read command line arguments
-    parser = argparse.ArgumentParser(description='Train a PDF GAN.')
-    parser.add_argument('runcard', action='store', default=None,
-                        help='A json file with the setup.')
-    parser.add_argument('--output', '-o', type=str, default=None,
-                        help='The output folder.', required=True)
-    parser.add_argument('--force', action='store_true')
-    parser.add_argument('--hyperopt', default=None, type=int,
-                        help='Enable hyperopt scan.')
-    parser.add_argument('--cluster', default=None, type=str,
-                        help='Enable cluster scan.')
-    parser.add_argument('--nreplicas', default=None, type=int,
-                        help='Define the number of input replicas.')
-    parser.add_argument('--pplot', default=None, type=int,
-                        help='Define the number of output replicas.')
-    parser.add_argument('--flavors', default=None, type=int,
-                        help='Choose the falvours.')
+    parser = argparse.ArgumentParser(description="Train a PDF GAN.")
+    parser.add_argument(
+        "runcard", action="store", default=None, help="A json file with the setup."
+    )
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        default=None,
+        help="The output folder.",
+        required=True,
+    )
+    parser.add_argument("--force", action="store_true")
+    parser.add_argument(
+        "--hyperopt", default=None, type=int, help="Enable hyperopt scan."
+    )
+    parser.add_argument(
+        "--cluster", default=None, type=str, help="Enable cluster scan."
+    )
+    parser.add_argument(
+        "--nreplicas",
+        default=None,
+        type=int,
+        help="Define the number of input replicas.",
+    )
+    parser.add_argument(
+        "--pplot", default=None, type=int, help="Define the number of output replicas."
+    )
+    parser.add_argument(
+        "--flavors", default=None, type=int, help="Choose the falvours."
+    )
     # parser.add_argument('--timeline', action='store_true')
     args = parser.parse_args()
 
     # check the runcard
     if not os.path.isfile(args.runcard):
-        raise ValueError('Invalid runcard: not a file.')
+        raise ValueError("Invalid runcard: not a file.")
     if args.force:
-        print('WARNING: Running with --force option will overwrite existing model.')
+        print("WARNING: Running with --force option will overwrite existing model.")
 
     # prepare the output folder
     if not os.path.exists(args.output):
@@ -41,47 +57,47 @@ def main():
         os.mkdir(args.output)
     else:
         raise Exception(f'{args.output} already exists, use "--force" to overwrite.')
-    out = args.output.strip('/')
+    out = args.output.strip("/")
 
     # copy runcard to output folder
-    shutil.copyfile(args.runcard, f'{out}/input-runcard.json')
+    shutil.copyfile(args.runcard, f"{out}/input-runcard.json")
 
-    print('[+] Loading runcard')
+    print("[+] Loading runcard")
     hps = load_yaml(args.runcard)
-    hps['verbose'] = False
-    hps['save_output'] = out
+    hps["verbose"] = False
+    hps["save_output"] = out
 
     # Define the number of input replicas
     if args.nreplicas == None:
-        hps['input_replicas'] = 1
-    elif args.nreplicas > 0 :
-        hps['input_replicas'] = args.nreplicas
+        hps["input_replicas"] = 1
+    elif args.nreplicas > 0:
+        hps["input_replicas"] = args.nreplicas
     else:
-        raise Exception(f'{args.nreplicas} not valid. Value must be positive!!!')
+        raise Exception(f"{args.nreplicas} not valid. Value must be positive!!!")
 
     # Define the number of output replicas
     if args.pplot == None:
-        hps['out_replicas'] = hps['input_replicas']
+        hps["out_replicas"] = hps["input_replicas"]
     elif args.pplot > 0:
-        hps['out_replicas'] = args.pplot
+        hps["out_replicas"] = args.pplot
     else:
-        raise Exception(f'{args.pplot} not valid. Value must be positive!!!')
+        raise Exception(f"{args.pplot} not valid. Value must be positive!!!")
 
     # Check the input flavor
     if args.flavors == None:
-        hps['fl'] = 1       # Take the u quark as a default
-    elif args.flavors in [1,2,3,21]:
-        hps['fl'] = args.flavors
+        hps["fl"] = 1  # Take the u quark as a default
+    elif args.flavors in [1, 2, 3, 21]:
+        hps["fl"] = args.flavors
     else:
-        raise Exception(f'{args.falvors} not valid. Must be one of the particle IDs!!!')
+        raise Exception(f"{args.falvors} not valid. Must be one of the particle IDs!!!")
 
     # If hyperscan is set true
     if args.hyperopt:
-        hps['scan'] = True
+        hps["scan"] = True
         hps = run_hyperparameter_scan(hps, args.hyperopt, args.cluster, out)
 
     # Run the best Model and output log
-    hps['scan'] = False
-    hps['verbose'] = True
+    hps["scan"] = False
+    hps["verbose"] = True
 
     loss = hyper_train(hps)
