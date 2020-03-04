3d78f5fed7422c1a8d7990fdb0736d30aa8e648a
juacrumar <juacrumar@gmail.com>
2020-03-04 10:30:41 +0100

    added .pylintrc file, massaged a bit the input

--- src/wganpdfs/run.py
+++ src/wganpdfs/run.py
@@ -1,53 +1,70 @@
-import os, argparse, shutil
+"""
+    This file contains the main driver of the wganpdfs code
+"""
+
+import os
+import shutil
+import logging
+import argparse
 from wganpdfs.hyperscan import hyper_train, load_yaml, run_hyperparameter_scan
 
+logger = logging.getLogger(__name__)
 
-def main():
-    """
-    This is the Main controller.
-    It controlls the input parameters.
-    """
+ALLOWED_FLAVORS = [1, 2, 3, 21]
 
+
+def positive_int(value):
+    """ Checks that a given number is positive """
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(
+            f"Negative values are not allowed, received: {value}"
+        )
+    return ivalue
+
+
+def flavour_int(value):
+    """ Check whether the given flavour is among the allowed values """
+    ivalue = int(value)
+    if ivalue not in ALLOWED_FLAVORS:
+        raise argparse.ArgumentTypeError(
+            f"{ivalue} not allowed, allowed values: {ALLOWED_FLAVORS}"
+        )
+    return ivalue
+
+
+def argument_parser():
+    """
+        Parse the input arguments for wganpdfs
+    """
     # read command line arguments
     parser = argparse.ArgumentParser(description="Train a PDF GAN.")
+    parser.add_argument("runcard", help="A json file with the setup.")
+    parser.add_argument("-o", "--output", help="The output folder.", required=True)
+    parser.add_argument("-f", "--force", action="store_true")
+    parser.add_argument("--hyperopt", type=int, help="Enable hyperopt scan.")
+    parser.add_argument("--cluster", help="Enable cluster scan.")
     parser.add_argument(
-        "runcard", action="store", default=None, help="A json file with the setup."
-    )
-    parser.add_argument(
-        "--output",
-        "-o",
-        type=str,
-        default=None,
-        help="The output folder.",
-        required=True,
-    )
-    parser.add_argument("--force", action="store_true")
-    parser.add_argument(
-        "--hyperopt", default=None, type=int, help="Enable hyperopt scan."
-    )
-    parser.add_argument(
-        "--cluster", default=None, type=str, help="Enable cluster scan."
-    )
-    parser.add_argument(
+        "-n",
         "--nreplicas",
-        default=None,
-        type=int,
+        type=positive_int,
         help="Define the number of input replicas.",
     )
     parser.add_argument(
-        "--pplot", default=None, type=int, help="Define the number of output replicas."
+        "--pplot", type=positive_int, help="Define the number of output replicas."
     )
-    parser.add_argument(
-        "--flavors", default=None, type=int, help="Choose the falvours."
-    )
+    parser.add_argument("--flavors", type=flavour_int, help="Choose the flavours.")
     # parser.add_argument('--timeline', action='store_true')
     args = parser.parse_args()
+
+    # Sanitize the arguments
+    # Check that the flavours are allowed
 
     # check the runcard
     if not os.path.isfile(args.runcard):
         raise ValueError("Invalid runcard: not a file.")
     if args.force:
-        print("WARNING: Running with --force option will overwrite existing model.")
+        logger.warning("Running with --force option will overwrite existing model.")
 
     # prepare the output folder
     if not os.path.exists(args.output):
@@ -57,39 +74,45 @@ def main():
         os.mkdir(args.output)
     else:
         raise Exception(f'{args.output} already exists, use "--force" to overwrite.')
+
+    return args
+
+
+def main():
+    """
+    This is the Main controller.
+    It controlls the input parameters.
+    """
+
+    args = argument_parser()
+
     out = args.output.strip("/")
 
     # copy runcard to output folder
     shutil.copyfile(args.runcard, f"{out}/input-runcard.json")
 
-    print("[+] Loading runcard")
+    logger.info("[+] Loading runcard")
     hps = load_yaml(args.runcard)
     hps["verbose"] = False
     hps["save_output"] = out
 
     # Define the number of input replicas
-    if args.nreplicas == None:
+    if args.nreplicas is None:
         hps["input_replicas"] = 1
-    elif args.nreplicas > 0:
-        hps["input_replicas"] = args.nreplicas
     else:
-        raise Exception(f"{args.nreplicas} not valid. Value must be positive!!!")
+        hps["input_replicas"] = args.nreplicas
 
     # Define the number of output replicas
-    if args.pplot == None:
+    if args.pplot is None:
         hps["out_replicas"] = hps["input_replicas"]
-    elif args.pplot > 0:
-        hps["out_replicas"] = args.pplot
     else:
-        raise Exception(f"{args.pplot} not valid. Value must be positive!!!")
+        hps["out_replicas"] = args.pplot
 
     # Check the input flavor
-    if args.flavors == None:
+    if args.flavors is None:
         hps["fl"] = 1  # Take the u quark as a default
-    elif args.flavors in [1, 2, 3, 21]:
-        hps["fl"] = args.flavors
     else:
-        raise Exception(f"{args.falvors} not valid. Must be one of the particle IDs!!!")
+        hps["fl"] = args.flavors
 
     # If hyperscan is set true
     if args.hyperopt:
