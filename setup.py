1d8d8431914ade97bc2ec16dda8989bb5f746618
Radonirinaunimi <tanjona.rabemananjara@unimi.it>
2020-08-19 20:28:36 +0200

    Added new dependency

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ TESTS_REQUIEREMENTS = ["pytest", "pytest-cov"]
 # Depending on the documents more dependencies can be added
 DOCS_REQUIEREMENTS = ["recommonmark", "sphinx_rtd_theme", "sphinxcontrib-bibtex"]
 # Dependencies for the packages
-PACKAGE_REQUIEREMENTS = ["hyperopt", "numpy", "tensorflow", "scipy", "matplotlib"]
+PACKAGE_REQUIEREMENTS = ["tensorflow", "numpy", "scipy", "tqdm", "hyperopt", "matplotlib"]
 
 
 # Check python version
