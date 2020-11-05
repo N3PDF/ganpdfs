886cc879ab776b3ae207f1424383f443b3dc1a06
Tanjona R. Rabemananjara <tanjona.rabemananjara@unimi.it>
2020-11-05 09:57:39 +0100

    Try to add custom SMR layer that is not working

--- src/ganpdfs/custom.py
+++ src/ganpdfs/custom.py
@@ -1,20 +1,17 @@
-import os
 import numpy as np
-
 import tensorflow as tf
-from scipy import stats
+
 from tensorflow.keras import initializers
 from tensorflow.keras import backend as K
 from tensorflow.keras.layers import Layer
 from tensorflow.keras.constraints import Constraint
-from tensorflow.keras.losses import BinaryCrossentropy
 
 
 def wasserstein_loss(y_true, y_pred):
-    """Compute the wasserstein loss from the true and predictions. The loss function 
-    is implemented by multiplying the expected label for each sample by the predicted 
-    score (element wise), then calculating the mean. For more details, see
-    https://arxiv.org/abs/1506.05439
+    """Compute the wasserstein loss from the true and predictions. The
+    loss function is implemented by multiplying the expected label for
+    each sample by the predicted score (element wise), then calculating
+    the mean. For more details, see https://arxiv.org/abs/1506.05439
 
     Parameters
     ----------
@@ -26,6 +23,43 @@ def wasserstein_loss(y_true, y_pred):
         samples
     """
     return K.mean(y_true * y_pred)
+
+
+def compute_smr(pdf, inverse_x):
+    """Impose sum rules on a bacth of PDF by performing the following:
+    -   Compute the sum of the PDF over x while dividing the basis by x
+        except for `sigma` and `g`.
+    -   Take the x grid and expand the dimension such that the output
+        shape is exactly the same as the batched PDF such that both can
+        just be multiplied together.
+
+    Parameters
+    ----------
+    pdf: tf
+        Batches of PDF
+    xgrid: tf
+        Grid of x values
+
+    Returns
+    -------
+    """
+    sum_overx = K.sum(pdf * inverse_x, axis=2)
+    # Compute Normalization Factor
+    # ones = K.ones(2)
+    # pdf_arr, sum_overx_arr = K.eval(pdf), K.eval(sum_overx)
+    # vv_norm = [3 / v[2] for v in sum_overx]
+    # v3_norm = [1 / v[3] for v in sum_overx]
+    # v8_norm = [3 / v[4] for v in sum_overx]
+    # gg_norm = [(1 - v[0]) / v[1] for v in sum_overx_arr]
+    # norm_arr = np.array([gg_norm, vv_norm, v3_norm, v8_norm, ones, ones, ones, ones])
+    # Perform Multiplication
+    # pdf_norm = []
+    # for p, n in zip(pdf_arr, norm_arr):
+    #     normalized = (p.T * n.T).T
+    #     pdf_norm.append(normalized)
+    # pdf_norm = np.array(pdf_norm)
+    # return K.constant(pdf_norm)
+    return sum_overx
 
 
 class ClipConstraint(Constraint):
@@ -43,6 +77,64 @@ class ClipConstraint(Constraint):
         return {"clip_value": self.clip_value}
 
 
+class ImposeSumRules(Layer):
+    """Impose sum rules on PDF.
+
+    Parameters
+    ----------
+    xgrid: np.array
+        Array of x values
+    """
+
+    def __init__(self, xgrid, **kwargs):
+        self.xgrid = K.constant(xgrid)
+        super(ImposeSumRules, self).__init__(**kwargs)
+
+    def compute_output_shape(self, input_shape):
+        return input_shape
+    
+    def build(self, input_shape):
+        nb_basis_to_divide = 6
+        arr_ones = K.ones((2, self.xgrid.shape[0]))
+        xgrid = K.reshape(self.xgrid, shape=(1, self.xgrid.shape[0]))
+        inverse_x = K.tile(1 / xgrid, (nb_basis_to_divide, 1))
+        self.inverse_x = K.concatenate([arr_ones, inverse_x], axis=0)
+        # super(ImposeSumRules, self).build(input_shape)
+
+    def call(self, previous_layer):
+        if previous_layer.shape[0] is not None:
+            # Multiplication along the last two axes
+            xsummed = K.sum(previous_layer * self.inverse_x, axis=2)        # Shape=(batches, evol)
+            xtransposed = K.permute_dimensions(xsummed, pattern=(1, 0))     # Shape=(evol, batches)
+            gg_norm = (1 - xtransposed[0]) / xtransposed[1]
+            vv_norm = 3 / xtransposed[2]
+            v3_norm = 1 / xtransposed[3]
+            v8_norm = 3 / xtransposed[4]
+            normalized_pdf = K.concatenate(
+                    [
+                        [xtransposed[0]],
+                        [gg_norm],
+                        [vv_norm],
+                        [v3_norm],
+                        [v8_norm],
+                        [xtransposed[5]],
+                        [xtransposed[6]],
+                        [xtransposed[7]]
+                    ],
+                    axis=0
+            )   # Shape=(evol, batches) 
+            # Permute the dimensions
+            pdfpermute = K.permute_dimensions(
+                    previous_layer,
+                    pattern=(2, 1, 0)
+            )
+            previous_layer = K.permute_dimensions(
+                    pdfpermute * normalized_pdf,
+                    pattern=(0, 1, 2)
+            )
+        return previous_layer
+
+
 class ConvPDF(Layer):
     """Convolute the output of the previous layer with
     a subsample of the input/prior replica.
@@ -53,14 +145,13 @@ class ConvPDF(Layer):
         Array of PDF grids
     """
 
-    def __init__(self, pdf, trainable=True, **kwargs):
+    def __init__(self, pdf, **kwargs):
         index = np.random.randint(pdf.shape[0])
         self.pdf = K.constant(pdf[index])
-        self.trainable = trainable
         super(ConvPDF, self).__init__(**kwargs)
 
     def compute_output_shape(self, input_shape):
-        return (None, self.pdf.shape[1], self.pdf.shape[2])
+        return input_shape
 
     def call(self, previous_layer):
         mult = previous_layer * self.pdf
@@ -68,7 +159,7 @@ class ConvPDF(Layer):
 
 
 class ConvXgrid(Layer):
-    """Convolute the output of the prvious layer with the 
+    """Convolute the output of the prvious layer with the
     input x-grid."""
 
     def __init__(self, output_dim, xval, kernel_initializer="glorot_uniform", **kwargs):
@@ -98,7 +189,6 @@ class ConvXgrid(Layer):
 
 
 class PreprocessFit(Layer):
-
     """Add preprocessing to output of the previous layer. This probably assures
     the PDF-like behavior of the generated samples.
 
@@ -126,7 +216,6 @@ class PreprocessFit(Layer):
     def compute_output_shape(self, input_shape):
         return input_shape
 
-    # @tf.function
     def call(self, pdf):
         xres = self.xval ** self.kernel[0] * (1 - self.xval) ** self.kernel[1]
         return pdf * xres
